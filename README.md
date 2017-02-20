bf2d128ed1c7c7b25bec91ed40dd50dc5a4dc192
lhtin <lehuading@qq.com>
2017-02-20 16:08:28 +0800

    init geetest2-npm

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # geetest2-npm
 
 Geetest 2.0 for NPM
+
+NOTE: geetest2 does not require any 3rd library
+
+## install
+
+```sh
+npm install geetest2 --save
+```
+
+## use
+
+```js
+var Geetest = require('geetest2');
+
+// now can use Geetest constructor
+// for more detail, refer the demo (dir demo/)
+```
+
+## run demo
+
+```sh
+npm install
+npm start
+// access http://localhost:9991
+```
+
+
---
+++ demo/app/index.js
@@ -0,0 +1,46 @@
+var Geetest = require('../../dist/geetest'); // geetest2 on npm
+var $ = require('jquery'); // just use jquery for ajax
+
+var login = function (result) {
+    $.post('/api/gt-validate', {
+        geetest_challenge: result.geetest_challenge,
+        geetest_validate: result.geetest_validate,
+        geetest_seccode: result.geetest_seccode,
+        username: $('#username').val(),
+        password: $('#password').val()
+    }, function (data) {
+        if (data.status === 'success') {
+            alert('login succeed');
+        } else if (data.status === 'fail') {
+            alert('please complete verification');
+        } else if (data.status === 'error') {
+            alert(data.msg);
+        }
+    })
+};
+
+$.get('/api/gt-register', null, function (data) {
+    var captcha = new Geetest({
+        gt: data.gt,
+        challenge: data.challenge,
+        offline: !data.success,
+        product: 'embed',
+        lang: 'en'
+    });
+
+    captcha.appendTo('#captcha');
+
+    captcha.onReady(function () {
+        $('#tip').hide();
+    });
+
+    $('#submit').click(function (e) {
+        var result = captcha.getValidate();
+        if (result) {
+            login(result);
+        } else {
+            alert('please complete verification');
+        }
+        e.preventDefault();
+    })
+});
